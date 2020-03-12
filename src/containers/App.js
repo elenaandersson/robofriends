1ad695c19cdd75963baf82f68493039d70de97b0
elenaandersson <elena.andersson@live.se>
2020-03-12 15:32:57 +0100

    This is the redux-react update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import CardList from '../components/CardList.js';
-import {robots} from '../components/robots.js';
 import SearchBox from '../components/SearchBox.js';
 import './App.css';
 
-// This is the app component
+import {setSearchField, requestRobots} from '../actions.js';
+
+const mapStateToProps = state => {
+  return {
+  searchField: state.searchRobots.searchField,
+  robots: state.requestRobots.robots,
+  isPending: state.requestRobots.isPending,
+  error: state.requestRobots.error
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
+
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      // The app has to states, robots and searchfield
-      //It is things that changes in the App
-      // depending on the input
-      robots: robots,
-      searchfield: ''
-    }
+  componentDidMount() {
+    this.props.onRequestRobots();
   }
-  onSearchChange = (event) =>{
-    this.setState ({searchfield: event.target.value})
-  }
+
   render () {
-    const {robots, searchfield} = this.state;
+    const {searchField, onSearchChange, robots, isPending} = this.props;
     const filterRobot = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    /*This is the DOM or VRDom, that you will see on the browser
-    A searchbox that update when the input event accures
-    The robotcards, filters robots when input event accures
-    */
-    return (
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return isPending ?
+      <h1> Loading </h1> :
+      (
       <fragment>
         <main className='tc'>
         <h1>Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <br/>
         <CardList robots={ filterRobot}/>
         </main>
@@ -41,4 +47,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
